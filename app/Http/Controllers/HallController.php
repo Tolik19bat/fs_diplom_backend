3e56e9249fda44e2cfc1d976c61f29e3dc4f8184
Anatoly Batov <play.hoffa@gmail.com>
2024-12-04 22:41:32 +0400

    Hall

---
+++ app/Http/Controllers/HallController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Hall;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\HallRequest;
+
+class HallController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Hall::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(HallRequest $request)
+    {
+        return Hall::query()->create($request->validated());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(int $hallId)
+    {
+        return Hall::query()->findOrFail($hallId);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(HallRequest $request, int $hallId)
+    {
+        $hall = Hall::query()->findOrFail($hallId);
+        $hall->fill($request->validated);
+        return $hall->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(int $hallId)
+    {
+        $hall = Hall::query()->findOrFail($hallId);
+        $hall->delete();
+        return 'ok';
+    }
+}
---
+++ app/Models/Hall.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Hall extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'ticket_price',
+        'vip_ticket_price',
+        'sales'
+
+    ];
+}
