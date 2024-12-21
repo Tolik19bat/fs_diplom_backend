7be67515458a1d6cd4b9a483f9ddf9de0c5fad74
Anatoly Batov <play.hoffa@gmail.com>
2024-12-21 16:27:37 +0400

    HallController

--- app/Http/Controllers/HallController.php
+++ app/Http/Controllers/HallController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\PricesHallRequest;
+use App\Http\Requests\SalesRequest;
 use App\Models\Hall;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\HallRequest;
--- app/Http/Requests/SalesRequest.php
+++ app/Http/Requests/SalesRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class HallRequest extends FormRequest
+class SalesRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -22,9 +22,6 @@ class HallRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string'],
-            'ticket_price' => ['required', 'integer'],
-            'vip_ticket_price' => ['required', 'integer'],
             'sales' => ['required', 'boolean'],
         ];
     }
