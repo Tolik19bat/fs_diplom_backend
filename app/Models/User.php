b8f34ab4f1d359ed5b87cfe34feeb90b0ddd8203
Anatoly Batov <play.hoffa@gmail.com>
2025-01-11 07:00:57 +0400

    User

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,10 +31,6 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
-        'created_at',
-        'updated_at',
-        'id',
-        'email_verified_at',
     ];
 
     /**
