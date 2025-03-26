b347243ebac1499e5c158a03022b489dbd8491cd
Anatoly Batov <play.hoffa@gmail.com>
2025-03-26 18:27:37 +0400

    api

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HallController;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\ChairController;
+use App\Http\Controllers\SeanceController;
+use App\Http\Controllers\TicketController;
+use App\Http\Controllers\QrcodeController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::middleware('throttle:limitRequest')->group(function () {
-    Route::post('register', [App\Http\Controllers\AuthController::class, 'register']);
-    Route::post('login', [App\Http\Controllers\AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
     Route::post('/tokens/create', function (Request $request) {
         $token = $request->user()->createToken($request->token_name);
         return ['token' => $token->plainTextToken];
@@ -17,26 +24,33 @@ Route::middleware('throttle:limitRequest')->group(function () {
 });
 
 Route::middleware(['auth:sanctum', 'throttle:limitRequest'])->group(function () {
-    Route::apiResource('/hall', App\Http\Controllers\HallController::class);
-    Route::put('/chair', [App\Http\Controllers\ChairController::class, 'updateChairs']);
-    Route::put('/hall/prices/{id}', [App\Http\Controllers\HallController::class, 'updatePrices']);
-    Route::get('/hall/{hallId}/seances', [App\Http\Controllers\HallController::class, 'getSeances']);
-    Route::put('/hall/{hallId}/sales', [App\Http\Controllers\HallController::class, 'setSales']);
-    Route::apiResource('/movie', App\Http\Controllers\MovieController::class);
-    Route::delete('/seance/all/{movieId}', [App\Http\Controllers\SeanceController::class, 'deleteAll']);
-    Route::get('/logout', [App\Http\Controllers\AuthController::class, 'logout']);
+    Route::apiResource('/hall', HallController::class);
+    // Route::apiResource('/movie', MovieController::class);
+
+    Route::get('/movie', [MovieController::class, 'index']); // Вернуть все фильмы
+    Route::post('/movie', [MovieController::class, 'store']); // Создать новый фильм
+    Route::get('/movie/{movieId}', [MovieController::class, 'show']); // Получить данные о фильме
+    Route::put('/movie/{movieId}', [MovieController::class, 'update']); // Обновить фильм
+    Route::delete('/movies/{movieId}', [MovieController::class, 'destroy']); // Удалить фильм
+
+    Route::put('/chair', [ChairController::class, 'updateChairs']);
+    Route::put('/hall/prices/{id}', [HallController::class, 'updatePrices']);
+    Route::get('/hall/{hallId}/seances', [HallController::class, 'getSeances']);
+    Route::put('/hall/{hallId}/sales', [HallController::class, 'setSales']);
+    Route::delete('/seance/all/{movieId}', [SeanceController::class, 'deleteAll']);
+    Route::get('/logout', [AuthController::class, 'logout']);
 });
 
 Route::middleware('throttle:limitRequest')->group(function () {
-    Route::get('/hall/{hallId}/chairs', [App\Http\Controllers\HallController::class, 'getChairs']);
+    Route::get('/hall/{hallId}/chairs', [HallController::class, 'getChairs']);
 
-    Route::get('/chair/seance/{seanceId}/date/{date}', [App\Http\Controllers\ChairController::class, 'getBySeanceIdAndDate']);
+    Route::get('/chair/seance/{seanceId}/date/{date}', [ChairController::class, 'getBySeanceIdAndDate']);
 
-    Route::apiResource('/seance', App\Http\Controllers\SeanceController::class);
-    Route::apiResource('/chair', App\Http\Controllers\ChairController::class);
-    Route::get('/hall/{hallId}/seances/{movieId}', [App\Http\Controllers\HallController::class, 'getSeances']);
-    Route::get('/movie/date/{date}', [App\Http\Controllers\MovieController::class, 'getByDate']);
-    Route::get('/hall/seances/available', [App\Http\Controllers\HallController::class, 'getSeancesAvailable']);
-    Route::post('/ticket', [App\Http\Controllers\TicketController::class, 'store']);
-    Route::post("/qrcode", [App\Http\Controllers\QrcodeController::class, 'getQrcode']);
+    Route::apiResource('/seance', SeanceController::class);
+    Route::apiResource('/chair', ChairController::class);
+    Route::get('/hall/{hallId}/seances/{movieId}', [HallController::class, 'getSeances']);
+    Route::get('/movie/date/{date}', [MovieController::class, 'getByDate']);
+    Route::get('/hall/seances/available', [HallController::class, 'getSeancesAvailable']);
+    Route::post('/ticket', [TicketController::class, 'store']);
+    Route::post("/qrcode", [QrcodeController::class, 'getQrcode']);
 });
