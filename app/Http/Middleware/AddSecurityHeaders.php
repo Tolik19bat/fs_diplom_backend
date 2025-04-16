d13fcd09246d6db41014e68bc1d9746bd2dca719
Anatoly Batov <play.hoffa@gmail.com>
2025-04-16 22:56:33 +0400

    AddSecurityHeaders

--- app/Http/Middleware/AddSecurityHeaders.php
+++ app/Http/Middleware/AddSecurityHeaders.php
@@ -8,16 +8,22 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AddSecurityHeaders
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next): Response
     {
         $response = $next($request);
+
+        // Устанавливаем заголовки безопасности
         $response->headers->set('X-Content-Type-Options', 'nosniff');
+        // $response->headers->set('Content-Security-Policy', "frame-ancestors 'self'");
         
+        // Если нужно оставить X-Frame-Options для совместимости (необязательно)
+        // $response->headers->set('X-Frame-Options', 'SAMEORIGIN');
+
+        // Устанавливаем Content-Type
+        if (str_contains($response->headers->get('Content-Type'), 'application/json')) {
+            $response->headers->set('Content-Type', 'application/json; charset=utf-8');
+        }
+
         return $response;
     }
 }
